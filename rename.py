64b5ef808d6b8a55d6c0686529df1f8deede8280
7gahunguflorian <120568555+7gahunguflorian@users.noreply.github.com>
2025-06-08 19:11:54 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -13,6 +13,6 @@ for filename in os.listdir(folder):
         new_filename = prefix + filename
         new_path = os.path.join(folder, new_filename)
         os.rename(old_path, new_path)
-        print(f"✅ Renommé : {filename} → {new_filename}")
+        print(f"Renommé : {filename} → {new_filename}")
 
 print("Tous les fichiers ont été renommés avec succès.")
